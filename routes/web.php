8381580ba934b8762aa98c3696dd480b8d7b9f41
wandersom <wandersompsn2017@gmail.com>
2023-11-01 00:27:13 +0000

    Minhas alterações servidor produção

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 Route::get('/', [ArtigoController::class, 'index'])->name('poastagem.todos');
 
